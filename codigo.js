dae890a32c9a148db0fc501a29a3b2c0a59bca13
mauxi95 <mauxi95natera@gmail.com>
2023-05-31 22:12:28 -0300

    Js?

--- codigo.js
+++ codigo.js
@@ -1,19 +1,6 @@
+function alertaboton () { 
+  alert ("Su formulario ha sido enviado con éxito, en caso de avanzar con tu perfil, nos estaremos comunicando a la brevedad")
+}
 
-    document.getElementById("myForm").addEventListener("submit", function(event) {
-      // Detiene el envío del formulario
-      event.preventDefault();
+onclick="alertaboton();" 
 
-      // Verifica las condiciones del formulario
-      var name = document.getElementById("name").value;
-      var email = document.getElementById("email").value;
-
-      if (name === "" || email === "") {
-        // Muestra una alerta si algún campo está vacío
-        alert("Por favor, completa todos los campos");
-      } else {
-        // Enviar el formulario si todas las condiciones se cumplen
-        // Aquí puedes agregar el código para enviar el formulario a tu servidor
-        alert("Formulario enviado correctamente");
-        document.getElementById("myForm").submit();
-      }
-    });
